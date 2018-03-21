d43afde58dec9f2d512470983d40f66cd331af17
Johnson Earls <johnson.earls@oracle.com>
2018-03-21 23:18:40 +0000

    Add simple `spec` tests
    Run `rspec` from the top level directory to test.
    Assumes you have the `rspec` gem version 3.x
    installed (with its dependencies), plus the
    `puppetlabs_spec_helper` gem version 2.x.
    (Sorry, no Gemfile created for this)

---
+++ spec/types/echo_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+
+describe 'echo', :type => :type do
+  let(:my_type) { Puppet::Type.type(:echo) }
+  let(:default_title) { 'TestMessage' }
+
+  context 'with default parameters' do
+    it 'should default to using the name as the message' do
+      expect(Puppet).to receive(:notice).with('/Echo[TestMessage]/message: TestMessage')
+      my_type.new( { name: default_title } )
+    end
+  end
+
+  context 'with a specific message' do
+    it 'should send that message' do
+      expect(Puppet).to receive(:notice).with('/Echo[TestMessage]/message: test message')
+      my_type.new( { name: default_title, message: 'test message' } )
+    end
+  end
+
+  context 'with `withpath` set to false' do
+    it 'should not show the resource path' do
+      expect(Puppet).to receive(:notice).with('TestMessage')
+      my_type.new( { name: default_title, withpath: false } )
+    end
+  end
+
+  context 'with `refreshonly` set to `true`' do
+    it 'should not display anything' do
+      expect(Puppet).not_to receive(:notice)
+      my_type.new( { name: default_title, refreshonly: true} )
+    end
+  end
+
+  # TODO: figure out how to test the refresh notification
+
+end
