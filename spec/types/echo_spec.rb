8b75e6395813aeaf7eaa32f9d8b34ea0be448b28
Bert Hajee <bert.hajee@enterprisemodules.com>
2020-10-28 17:15:14 +0100

    Added support for  the schedule

--- spec/types/echo_spec.rb
+++ spec/types/echo_spec.rb
@@ -1,7 +1,8 @@
 require 'spec_helper'
-
+# rubocop: disable RSpec/VerifiedDoubles, RSpec/AnyInstance
 describe 'echo', type: :type do
   let(:my_type) { Puppet::Type.type(:echo) }
+
   let(:default_title) { 'TestMessage' }
 
   context 'with default parameters' do
@@ -37,6 +38,31 @@ describe 'echo', type: :type do
     it 'shows a critical log message' do
       expect(Puppet).to receive(:crit).with('/Echo[TestMessage]/message: TestMessage')
       my_type.new(name: default_title, loglevel: 'crit')
+    end
+  end
+
+  context 'with schedule set`' do
+    context 'unknown schedule' do
+      it 'fails with an error' do
+        catalog = double('catalog')
+        expect(catalog).to receive(:resource).and_return(nil)
+        allow_any_instance_of(my_type).to receive(:catalog).and_return(catalog)
+        expect { my_type.new(name: default_title, message: 'test message', schedule: 'unknown') }.to raise_error(RuntimeError, %r{Could not find schedule unknown})
+      end
+    end
+
+    context 'within known schedule' do
+      it 'sends that message' do
+        allow_any_instance_of(my_type).to receive(:scheduled?).and_return(true)
+        my_type.new(name: default_title, message: 'test message', schedule: 'within')
+      end
+    end
+
+    context 'outside known schedule' do
+      it 'doesn\'t sends a message' do
+        allow_any_instance_of(my_type).to receive(:scheduled?).and_return(false)
+        my_type.new(name: default_title, message: 'test message', schedule: 'outside')
+      end
     end
   end
 end
